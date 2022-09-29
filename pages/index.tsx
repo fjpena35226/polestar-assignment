d0fa2cc841a8505285a5fdaef476f4123904cf22
fjpena35226 <fjpena35226@gmail.com>
2022-09-29 11:27:50 +0200

    Creating index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
+import { ChangeEvent, useEffect, useState } from 'react'
 import type {
   GetServerSideProps,
   NextPage,
   InferGetServerSidePropsType,
 } from 'next'
+import SearchIcon from '@mui/icons-material/Search'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
+import { Alert, Grid, InputAdornment, Snackbar, TextField } from '@mui/material'
 
-import { searchPhotos } from 'helpers/flickr/queries/photos'
-import { getPhotoUrl } from 'helpers/flickr/Flickr'
-import { PhotoApi } from 'helpers/flickr/types/photo'
+import { searchPhotos, useSearchPhotos } from 'helpers/flickr/queries/photos'
+import Gallery from 'components/gallery/Gallery'
+import useAlert from 'hooks/useAlert'
+import ScrollToTop from 'components/scrollToTop/ScrollToTop'
+import useDebounce from 'hooks/useDebounce'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const photos = await searchPhotos({ text: 'teach' })
+  const photos = await searchPhotos({})
   return {
     props: {
-      photos: photos,
+      photos,
     },
   }
 }
@@ -20,12 +27,100 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 const Home: NextPage = ({
   photos,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const router = useRouter()
+  const [search, setSearch] = useState('')
+  const debouncedSearch = useDebounce(search)
+
+  const { isLoading, isError, error, data, fetchNextPage, hasNextPage } =
+    useSearchPhotos({ search: debouncedSearch, initialData: photos })
+
+  const { showAlert, closeAlert, alertOpen, alertMessage, alertColor } =
+    useAlert()
+
+  const handleOnChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const { value } = evt.currentTarget
+    setSearch(value)
+  }
+
+  useEffect(() => {
+    const { searchText } = router?.query
+    if (searchText && searchText !== search) setSearch(searchText as string)
+  }, [router.query])
+
+  useEffect(() => {
+    if (isError) {
+      showAlert({
+        alertMessage: 'Something went wrong trying to get the images',
+        alertColor: 'error',
+      })
+      console.error(error)
+    }
+  }, [isError])
+
   return (
-    <div>
-      {photos.map((photo: PhotoApi) => (
-        <img src={getPhotoUrl(photo)} width={500} height={500}/>
-      ))}
-    </div>
+    <>
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={6000}
+        onClose={() => closeAlert({})}
+      >
+        <Alert
+          onClose={() => closeAlert({})}
+          severity={alertColor || 'info'}
+          sx={{ width: '100%' }}
+        >
+          {alertMessage}
+        </Alert>
+      </Snackbar>
+      <Grid
+        container
+        justifyContent='center'
+        alignItems='center'
+        sx={{ paddingX: { xs: 4, lg: 10 }, paddingY: 5 }}
+      >
+        <Grid
+          item
+          container
+          xs={12}
+          sx={{ marginBottom: 4, display: 'flex', justifyContent: 'center' }}
+        >
+          <Grid
+            item
+            xs={6}
+            sm={4}
+            md={2}
+            lg={1}
+            sx={{ position: 'relative', minHeight: '45px', '& img': { objectFit: 'cover' } }}
+          >
+            <Image src='/img/flickr-logo-brandlogos.net_.png' layout='fill' />
+          </Grid>
+          <Grid item xs={12} sm={8} md={10}>
+            <TextField
+              placeholder='Search photos'
+              name='search'
+              aria-label='search'
+              value={search || ''}
+              onChange={handleOnChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position='start'>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              sx={{ width: '100%' }}
+            />
+          </Grid>
+        </Grid>
+        <Gallery
+          isLoading={isLoading}
+          data={data}
+          onLoadMore={fetchNextPage}
+          hasMore={hasNextPage}
+        />
+      </Grid>
+      <ScrollToTop />
+    </>
   )
 }
 
